a00dac65651a82bb2557521bb7db0f855e41aa68
Mario Galindo <mariogalindo89@gmail.com>
2020-02-04 20:06:01 -0600

    added gitignore and mlab connection

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ if (process.env.NODE_ENV === "production") {
 app.use(routes);
 
 // Connect to the Mongo DB
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/reactreadinglist");
+mongoose.connect(process.env.MONGODB_URI || "mongodb://heroku_hczpl69k:jK@rE3vSwbSUfkD@ds035167.mlab.com:35167/heroku_hczpl69k");
 
 // Start the API server
 app.listen(PORT, function() {
